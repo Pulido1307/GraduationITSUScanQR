6782274dcf1850b6efdcb7ef1920bbc3a277d3d7
Pulido1307 <52903296+Pulido1307@users.noreply.github.com>
2020-11-03 01:45:41 -0600

    [ADD] Librería para scannear

---
+++ app/src/main/java/com/example/graduationitsuscanqr/Helpers/CaptureActivityPortrait.java
@@ -0,0 +1,5 @@
+package com.example.graduationitsuscanqr.Helpers;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+}
--- app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
+++ app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.graduationitsuscanqr.Helpers.CaptureActivityPortrait;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 public class MainActivity extends AppCompatActivity {
+
+    private IntentResult result= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void escanearQR()
+    {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intentIntegrator.setPrompt("Escanear código QR");
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.setCameraId(0);
+        intentIntegrator.setBeepEnabled(false);
+        intentIntegrator.setCaptureActivity(CaptureActivityPortrait.class);
+        intentIntegrator.setBarcodeImageEnabled(false);
+        intentIntegrator.initiateScan();
     }
 }
