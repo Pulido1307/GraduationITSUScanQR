de863b2806b526e188c9a9b97b031c704199b51f
Pulido1307 <52903296+Pulido1307@users.noreply.github.com>
2020-11-04 01:13:46 -0600

    [ADD] Dialog Cancel Invitation

--- app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
+++ app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
@@ -2,20 +2,35 @@ package com.example.graduationitsuscanqr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.graduationitsuscanqr.Helpers.CaptureActivityPortrait;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private IntentResult result= null;
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCancelInvitation();
+            }
+        });
     }
 
     private void escanearQR()
@@ -29,5 +44,38 @@ public class MainActivity extends AppCompatActivity {
         intentIntegrator.setCaptureActivity(CaptureActivityPortrait.class);
         intentIntegrator.setBarcodeImageEnabled(false);
         intentIntegrator.initiateScan();
+    }
+
+    private void showCancelInvitation(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_cancel_invitation, null);
+        builder.setView(view);
+
+        final AlertDialog dialogSearchInvitation =builder.create();
+        dialogSearchInvitation.setCancelable(false);
+        dialogSearchInvitation.show();
+        Button buttonBuscar = view.findViewById(R.id.buttonBuscar);
+        Button buttonClose = view.findViewById(R.id.buttonClose);
+
+        TextView textView_Ncontrol = view.findViewById(R.id.textView_Ncontrol);
+        TextView textView_Nombre = view.findViewById(R.id.textView_Nombre);
+        TextView textView_Carrera = view.findViewById(R.id.textView_Carrera);
+
+        buttonBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogSearchInvitation.dismiss();
+            }
+        });
     }
 }
