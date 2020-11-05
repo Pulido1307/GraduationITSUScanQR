33b983352c03f5e94eebc7c29a28a1c646a6b2aa
Salvador999 <salvadormorado2000@gmail.com>
2020-11-04 18:17:47 -0600

    [MOD] Modifique escaneo.

--- app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
+++ app/src/main/java/com/example/graduationitsuscanqr/MainActivity.java
@@ -1,34 +1,34 @@
 package com.example.graduationitsuscanqr;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.graduationitsuscanqr.Helpers.CaptureActivityPortrait;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private IntentResult result= null;
-    private Button button;
+    private Button button_scannear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        button_scannear = findViewById(R.id.button_scannear);
+        button_scannear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCancelInvitation();
+                escanearQR();
             }
         });
     }
@@ -46,36 +46,71 @@ public class MainActivity extends AppCompatActivity {
         intentIntegrator.initiateScan();
     }
 
-    private void showCancelInvitation(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
 
-        View view = inflater.inflate(R.layout.dialog_cancel_invitation, null);
-        builder.setView(view);
-
-        final AlertDialog dialogSearchInvitation =builder.create();
-        dialogSearchInvitation.setCancelable(false);
-        dialogSearchInvitation.show();
-        Button buttonBuscar = view.findViewById(R.id.buttonBuscar);
-        Button buttonClose = view.findViewById(R.id.buttonClose);
-
-        TextView textView_Ncontrol = view.findViewById(R.id.textView_Ncontrol);
-        TextView textView_Nombre = view.findViewById(R.id.textView_Nombre);
-        TextView textView_Carrera = view.findViewById(R.id.textView_Carrera);
-
-        buttonBuscar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
+        result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null)
+        {
+            if(result.getContents() != null)
+            {
+                Toast.makeText(MainActivity.this,result.getContents(),Toast.LENGTH_SHORT).show();
+                String values = result.getContents();
+                showCancelInvitation(values);
             }
-        });
-
-        buttonClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialogSearchInvitation.dismiss();
+            else
+            {
+                Toast.makeText(MainActivity.this,"Cancelaste escaneo.",Toast.LENGTH_SHORT).show();
             }
-        });
+        }
+    }
+
+    private void showCancelInvitation(String values){
+        final String array[] = values.split("\\|");
+        Toast.makeText(MainActivity.this,array.length+""+ array[0],Toast.LENGTH_SHORT).show();
+
+        if(array.length==3)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+
+            View view = inflater.inflate(R.layout.dialog_cancel_invitation, null);
+            builder.setView(view);
+
+            final AlertDialog dialogSearchInvitation =builder.create();
+            dialogSearchInvitation.setCancelable(false);
+                        Button buttonBuscar = view.findViewById(R.id.button_cancelar);
+            Button buttonClose = view.findViewById(R.id.buttonClose);
+
+            TextView textView_Ncontrol = view.findViewById(R.id.textView_Ncontrol);
+            TextView textView_Nombre = view.findViewById(R.id.textView_Nombre);
+            TextView textView_Carrera = view.findViewById(R.id.textView_Carrera);
+
+            textView_Ncontrol.setText("Número de control: "+array[0]);
+            textView_Nombre.setText("Nombre: "+array[1]);
+            textView_Carrera.setText("Carrera: "+array[2]);
+
+            dialogSearchInvitation.show();
+
+            buttonBuscar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                }
+            });
+
+            buttonClose.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dialogSearchInvitation.dismiss();
+                }
+            });
+        }
+        else
+        {
+
+        }
     }
 }
